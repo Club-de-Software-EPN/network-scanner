a1011d89893caa46efd46c2683b74485498b3d83
Juan José Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-07-28 19:03:19 -0500

    copy scripts to .webpack folder

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   /**
    * This is the main entry point for your application, it's the first file
@@ -11,4 +15,12 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json']
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: path.join(__dirname, 'src', 'library', 'ping.js'), to: path.join(__dirname, '.webpack', 'main')},
+        { from: path.join(__dirname, 'src', 'library', 'portScanner.js'), to: path.join(__dirname, '.webpack', 'main')},
+      ],
+    }),
+  ],
 };
